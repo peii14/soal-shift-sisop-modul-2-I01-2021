455cf70de48eba11c3fbe3f7f1f204ef67af6663
Gayuh Kautaman <gayuh14@yahoo.com>
2021-04-15 14:08:18 +0700

    done no 3

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -2,11 +2,12 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <curl/curl.h>
-#include <curl/easy.h>
+// #include <curl/curl.h>
+// #include <curl/easy.h>
 #include <unistd.h>
 #include <time.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 // run with gcc -o soal soal3.c -lcurl
@@ -42,6 +43,7 @@ void encrypt(char folder[]){
    snprintf(txtpath,sizeof txtpath,"./%s/status.txt",folder);
    fp  = fopen (txtpath, "w");
    int key=4;
+   //caesar cipher algorithm
    char message[]="Download Success", ch;
    for(int i = 0; message[i] != '\0'; ++i){
 		ch = message[i];
@@ -60,45 +62,87 @@ void encrypt(char folder[]){
 			message[i] = ch;
 		}
 	}
+   //write to txt
    printf("Download Success = %s\n",message);
    for (int i = 0;i<16; i++) 
       fputc(message[i], fp);
    fclose(fp);
 }
 
-int main(){
-   int createdir;
+int main(int argc, char **argv){
+   // int createdir;
    char image[30],folder[30],imagepath[100];
    Struct timeNow;
- 
-   //take present time
-   timeNow=getTime();
+   pid_t pid, sid, parent_id;        
+   parent_id = getpid();
+   int status;
+   pid = fork();
+   sid = setsid();
 
-   //concate folder name
-   snprintf(folder,sizeof folder,"%02d-%02d-%02d_%02d:%02d:%02d",timeNow.year,timeNow.month,timeNow.day,timeNow.hours,timeNow.minutes,timeNow.seconds);
+   //exit after finish forking
+   if (pid > 0)
+      exit(EXIT_SUCCESS);
 
-   //owner can read, group can read, other can read
-   //owner can execute, also group and other
-   createdir= mkdir(folder, S_IRUSR | S_IRGRP | S_IROTH | S_IXUSR | S_IXGRP | S_IXOTH | S_IWUSR | S_IWGRP | S_IWGRP);
-
-   // download image
-   for(int i=0;i<10;i++){
-      if(fork()==0){
-         printf("Duar\n");
-         //update present time in every loop
-         timeNow=getTime();
-         //image naming
-         snprintf(image,sizeof image,"%02d-%02d-%02d_%02d:%02d:%02d.jpg",timeNow.year,timeNow.month,timeNow.day,timeNow.hours,timeNow.minutes,timeNow.seconds);
-         snprintf(imagepath,sizeof imagepath,"./%s/%s",folder,image); 
-         //blank space due to bugs on wget that are redirecting to log file afterworth
-         execl("/usr/bin/wget", "wget", "-q","-O",imagepath, "https://picsum.photos/200.jpg","", NULL);
-      }
-      sleep(5);
+   FILE *killer;
+   killer = fopen("killer.sh", "w");
+   //when -z stop everything immediatly when -x stop after ziping
+   if (strcmp(argv[1], "-z") == 0)
+      fprintf(killer, "#!/bin/bash\nkill -9 -%d\n rm killer.sh", getpid());
+   else if (strcmp(argv[1], "-x") == 0)
+      fprintf(killer, "#!/bin/bash\nkill %d\n rm killer.sh", getpid());
+      
+   fclose(killer);
+   //to be executable bash
+   if(fork() == 0){
+      char *argvmod[] = {"chmod", "+x", "killer.sh", NULL};
+      execv("/bin/chmod", argvmod);
    }
-   encrypt(folder);
-   //zip file
-   char zipfile[40];
-   snprintf(zipfile,sizeof zipfile,"%s.zip",folder);
-   execl("/usr/bin/zip","zip","-rm",zipfile,folder,"-x","*.c",NULL);
+   //close read input and write output
+   close(STDIN_FILENO);
+   close(STDOUT_FILENO);
+
+   while(1){
+      //take present time
+      timeNow=getTime();
+      //concate folder name
+      snprintf(folder,sizeof folder,"%02d-%02d-%02d_%02d:%02d:%02d",timeNow.year,timeNow.month,timeNow.day,timeNow.hours,timeNow.minutes,timeNow.seconds);
+      
+      pid_t child1,child2,child3;
+      child1=fork();
+      int stat;
+      if(child1 == 0){
+         child2=fork();
+         if(child2==0){
+         //since create dir is not allowe until 15 april
+         // createdir= mkdir(folder, S_IRUSR | S_IRGRP | S_IROTH | S_IXUSR | S_IXGRP | S_IXOTH | S_IWUSR | S_IWGRP | S_IWGRP);
+            char *argvmk[] = {"mkdir", "-p", folder, NULL};
+            execv("/bin/mkdir",argvmk);
+         }
+         else{
+            while(wait(&status)>0);
+            // download image
+            for(int i=0;i<10;i++){
+               if(fork()==0){
+                  printf("Duar\n");
+                  //update present time in every loop
+                  timeNow=getTime();
+                  //image naming
+                  snprintf(image,sizeof image,"%02d-%02d-%02d_%02d:%02d:%02d.jpg",timeNow.year,timeNow.month,timeNow.day,timeNow.hours,timeNow.minutes,timeNow.seconds);
+                  snprintf(imagepath,sizeof imagepath,"./%s/%s",folder,image); 
+                  //blank space due to bugs on wget that are redirecting to log file afterworth
+                  //ga ngerti epoch cuy
+                  execl("/usr/bin/wget", "wget", "-q","-O",imagepath, "https://picsum.photos/200.jpg","", NULL);
+               }
+               sleep(5);
+            }
+            encrypt(folder);
+            //zip file
+            char zipfile[40];
+            snprintf(zipfile,sizeof zipfile,"%s.zip",folder);
+            execl("/usr/bin/zip","zip","-rm",zipfile,folder,"-x","*.c",NULL);
+         }
+      }
+      else sleep(40);
+   }
    return 0;
 }
